fcf82591d934688d00d810feb15fd1e3b7045ab7
varun2005cmd <136258677+varun2005cmd@users.noreply.github.com>
2024-09-30 15:59:04 +0530

    Create 16.cpp

---
+++ 16.cpp
@@ -0,0 +1,22 @@
+// Varun Pendem
+// PRN: 23070123149
+
+#include<iostream>
+using namespace std;
+int main(){
+    float n1 , n2 , ans;
+    cout<<"enter the values of numbers 1 and 2";
+    cin>>n1>>n2;
+    try{
+        if(n2==0){
+            throw n2;
+        }
+        else{
+            ans = n1/n2;
+            cout<<"Answer = "<<ans<<endl;
+        }
+    }
+    catch(float num){
+        cout<<"\nERROR: Division by "<<num<<endl;
+    }
+}
